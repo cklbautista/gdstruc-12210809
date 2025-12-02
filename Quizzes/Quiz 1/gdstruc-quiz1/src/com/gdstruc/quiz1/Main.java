f1280c67977354f4eab02b0da2aa730ff7906401
cklbautista <christiankent.bautista@benilde.edu.ph>
2025-12-02 19:50:55 +0800

    Quiz 1

---
+++ Quizzes/Quiz 1/gdstruc-quiz1/src/com/gdstruc/quiz1/Main.java
@@ -0,0 +1,73 @@
+package com.gdstruc.quiz1;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        int[] numbers = new int [10];
+
+        numbers[0] = 35;
+        numbers[1] = 69;
+        numbers[2] = 1;
+        numbers[3] = 10;
+        numbers[4] = -50;
+        numbers[5] = 320;
+        numbers[6] = 63;
+        numbers[7] = 58;
+        numbers[8] = 26;
+        numbers[9] = 13;
+
+        System.out.println("Before sort: ");
+        printArrayElements(numbers);
+
+        selectionSort(numbers);
+
+        System.out.println("\n\nAfter sort: ");
+        printArrayElements(numbers);
+    }
+
+    private static void bubbleSort(int[] arr)
+    {                                    // (-1) the value of the length of the array is what you declare
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            for (int i = 0; i < lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    int temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            int smallestIndex = 0 ;
+
+            for (int i = 1; i <= lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[smallestIndex])
+                {
+                    smallestIndex = i;
+                }
+            }
+
+            int temp = arr[lastSortedIndex];
+            arr[lastSortedIndex] = arr[smallestIndex];
+            arr[smallestIndex] = temp;
+        }
+    }
+
+    private static void printArrayElements(int[] arr)
+    {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
